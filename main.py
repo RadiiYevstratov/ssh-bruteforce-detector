109ea7b69936058d822f11412cb504854c2a1984
Radii Yevstratov <r.yevstratov.projects@gmail.com>
2026-07-12 18:07:38 +0200

    Bruteforce detector

---
+++ main.py
@@ -0,0 +1,53 @@
+TARGET = "Failed password"
+IP_dict ={}
+Exception = []
+
+
+def open_log(path):
+    with open(path, 'r') as file:
+        return file.readlines()
+    
+
+def get_ip(target_log):
+    try:
+        return target_log[target_log.index("from") + 1]
+    except ValueError:
+        return None
+
+def ip_control(ip_):
+    if ip_ not in IP_dict:
+        IP_dict[ip_] = 1
+    else:
+        IP_dict[ip_] += 1
+    
+
+def check_logs():
+    while True:
+        try:
+            logs = open_log(path = input("Logs path: "))
+            break
+        except FileNotFoundError:
+            print("Bad path")
+            
+            
+    for row in logs:
+        if TARGET in row:
+            ip = get_ip(row.split())
+            if  ip == None:
+                Exception.append(row)
+                pass
+            else:
+                ip_control(ip)
+    sort(IP_dict)
+
+def sort(IP_dict):
+    for key, value in sorted(IP_dict.items(), key=lambda item: item[1], reverse=True):
+        if value > 5:
+            print(f"IP: {key} | failed attempts: {value}")
+
+
+        
+                     
+check_logs()
+
+
