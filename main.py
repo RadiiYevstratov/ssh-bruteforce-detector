7394c8eaa0d65bd168d4627337c2a75cd9dc7372
Radii Yevstratov <r.yevstratov.projects@gmail.com>
2026-07-13 20:28:44 +0200

    Add sort of analyzing results

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ TARGET = "Failed password"
 
 
 def code_debug():
-    path, time = get_args()
+    path, time, failed_target = get_args()
     IP_dict, skipped = check_logs(path=path)
-    print(json.dumps(IP_dict, indent=2))
-    suspicious_ip(IP_dict, time)
+    result_dict = suspicious_ip(IP_dict, time)
+    sort_result(result_dict=result_dict, failed_target=failed_target)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Enter path of your log file", type=str)
-    parser.add_argument("-t", "--time", help="Enter period of time in minutes to check logs", type=int, default=5)
+    parser.add_argument("time", help="Enter period of time in minutes to check logs. Default number is 5", type=int, nargs="?", default=5) 
+    parser.add_argument("failed_target", help="Enter how many failed attempt need to be to include to result. Default number is 5", type=int, nargs="?", default=5) 
     args = parser.parse_args()
-    return args.path, args.time
+    return args.path, args.time, args.failed_target
 
 
 def check_logs(path):
@@ -89,10 +91,9 @@ def suspicious_ip(IP_dict, time):
             max_time = datetime.fromisoformat(str(starting_time)) + timedelta(minutes=time)
             finishing_time = max((finish_time for finish_time in time_list if datetime.fromisoformat(str(finish_time)) < max_time))
             failed_attempts = len(time_list[time_list.index(starting_time):time_list.index(finishing_time )])
-            print(failed_attempts, key)
             result_dict = result_save(result_dict, key, failed_attempts, starting_time, finishing_time)
 
-    print(json.dumps(result_dict, indent=2))
+    return result_dict
 
 def result_save(result_dict, key, failed_attempts, starting_time, finishing_time):
 
@@ -105,16 +106,26 @@ def result_save(result_dict, key, failed_attempts, starting_time, finishing_time
     return result_dict
             
 
+def sort_result(result_dict, failed_target):
+    pop_list = []
+    for  key, value_list in result_dict.items():
+        print(value_list)
+        if value_list[0] < failed_target:
+            pop_list.append(key)
+    
+    for remove_ip in pop_list:
+        result_dict.pop(remove_ip)
+    
+    print(result_dict)
 
 def convert_datetime(IP_dict):     
     for  value_list in IP_dict.values():
         for value in value_list:
             value = datetime.fromisoformat(str(value))
-            print(type(value))
 
     return IP_dict
 
 code_debug()
-# suspicious_ip(IP_dict, time)
+
 
 
